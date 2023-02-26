0e385bef9a481e8908a55e257c209ebfe8a3c2c2
marcela1966 <marcelaalasia15@gmail.com>
2023-02-26 20:42:47 -0300

    first commit

---
+++ CV_javascript.js
@@ -0,0 +1,16 @@
+document.getElementById("b_mostrar").addEventListener('click', function () 
+    { 
+    document.getElementById("link1").style.display='flex';
+    });
+
+document.getElementById("b_mostrar").addEventListener('click', function () { 
+    document.getElementById("link2").style.display='flex';
+    });
+    
+document.getElementById("b_ocultar").addEventListener('click', function () { 
+    document.getElementById("link1").style.display='none';
+    });
+
+document.getElementById("b_ocultar").addEventListener('click', function () { 
+    document.getElementById("link2").style.display='none';
+    });
